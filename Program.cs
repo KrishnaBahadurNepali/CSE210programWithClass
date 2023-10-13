88771f3c0bf1293ae1a8685a11789f5bf8a6cd92
KrishSymphonic <krishnabahadur.nepali@eemc.edu.np>
2023-10-13 10:17:19 +0545

    extra message is added

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+mport getpass
+
+class ScriptureVerse:
+    def __init__(self):
+        self.hidden = False
+        self.verse = ""
+
+    def write_verse(self, verse):
+        self.verse = verse
+        self.hidden = False
+
+    def hide_verse(self):
+        if not self.hidden:
+            self.verse = ""
+            self.hidden = True
+            print("Verse is now hidden.")
+
+    def reveal_verse(self):
+        if self.hidden:
+            password = getpass.getpass("Enter password to reveal the verse: ")
+            if password == "your_password_here":
+                self.hidden = False
+                print("Verse: " + self.verse)
+            else:
+                print("Incorrect password. Verse remains hidden.")
+
+# Create an instance of the ScriptureVerse class
+my_verse = ScriptureVerse()
+
+# Write a verse
+my_verse.write_verse("For God so loved the world, that he gave his only Son...")
+
+# Hide the verse
+my_verse.hide_verse()
+
+# Attempt to reveal the verse (enter the correct password when prompted)
+my_verse.reveal_verse()
